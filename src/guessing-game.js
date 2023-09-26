78347a6aef9a9086bba9878285c72d97b8f2deec
Marta <emeraldraven27@gmail.com>
2023-09-26 22:22:18 +0300

    feat: submit solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,25 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;    
     }
 
     guess() {
-
+// найти значение, отталкиваясь от диапазона min-max
+// находим середину
+    this.result = Math.ceil((this.min + this.max) / 2);
+    return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
+        return this.max;
     }
 
     greater() {
-
+        this.min = this.result;
+        return this.min;
     }
 }
 
